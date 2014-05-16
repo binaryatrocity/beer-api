19f2bbf1791d65160938bc78c2c016096ecca597
Brandon Cornejo <brandon.cornejo@gmail.com>
2014-05-16 16:01:56 -0500

    Sphinx documentation for Github pages.

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-import re
-
-SECRET_KEY = 'FreeBeer'
-
-# Database settings
-basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
